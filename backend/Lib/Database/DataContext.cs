690f3f8843f754b5f37384922db1dcf311e958c2
llitzlbauer <lukaslitzlbauer.ll@gmail.com>
2023-03-24 21:42:28 +0100

    setting up basic project structure; created code first database

---
+++ backend/Lib/Database/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Database
+{
+    public class DataContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TestDatabase;Integrated Security=True");
+        }
+    }
+}
---
+++ backend/Lib/Database/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lib.Database
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string? FirebaseUid { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+    }
+}
